df79bc2d1046a87346e1facb057c9aa6bf75d0ad
meshack-mbuvi <meshmbuvi@gmail.com>
2018-01-07 21:18:19 +0300

    Create package room. Add models for Room, Office and LivingSpace

---
+++ room/room.py
@@ -0,0 +1,37 @@
+class Room(object):
+
+	"""
+	Create general features of a general room
+	Each room has a certain capacity depending on its type(an office or a living room)
+	"""
+
+	def __init__(self):
+		self.capacity = '';
+
+
+class Office(Room):
+
+	"""
+	Add specific features to make a room an office.
+	An office has a name and a space for maximum of 4 people.
+	"""
+
+	def __init__(self, rname):
+
+		super(Office,self).__init__()
+		self.name = rname #office name
+		self.capacity = 4 #number of spaces per office
+
+class LivingSpace(Room):
+
+	"""
+	Add specific features to make a room a livingspace.
+	An office has a name and a space for maximum of 6 people.
+	"""
+
+	def __init__(self, rname):
+		
+		super(LivingSpace,self).__init__()
+		self.name = rname #office name
+		self.capacity = 6 #number of spaces per office	
+		
